d724b7182a1bfa4e2282ac617ea79ac8104ea848
nathan-nas <77968170+nathan-nas@users.noreply.github.com>
2023-03-29 13:36:33 +0700

    fix region

--- AppBackend/src/main/java/com/example/appbackend/config/AmazonConfig.java
+++ AppBackend/src/main/java/com/example/appbackend/config/AmazonConfig.java
@@ -1,8 +1,9 @@
 package com.example.appbackend.config;
 
-import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.*;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +22,7 @@ public class AmazonConfig {
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.AP_SOUTHEAST_1)
                 .build();
 
 
