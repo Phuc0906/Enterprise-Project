0c98ee6dca1fcfd17054241fcea420ed1520d150
ThaiThuan <s3877024@rmit.edu.vn>
2023-04-10 22:14:30 +0700

    Update Slider

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Slider from "./Slider";
 
 const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Slider></Slider>
         </div>
     );
 };
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -34,9 +34,9 @@ const NavBar = () => {
                 <span className="uppercase font-extralight">Sign in</span>
                 <div className="relative">
                     <ShoppingCartIcon className="w-6 h-6 "></ShoppingCartIcon>
-                    <span class="absolute flex h-5 w-5 top-0 right-0 translate-x-1/2 translate-y-[-70%]">
-                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                        <span class="relative inline-flex rounded-full h-5 w-5 bg-sky-500 items-center justify-center text-white text-[9px]">
+                    <span className="absolute flex h-5 w-5 top-0 right-0 translate-x-1/2 translate-y-[-70%]">
+                        <span className="absolute inline-flex w-full h-full rounded-full opacity-75 animate-ping bg-sky-400"></span>
+                        <span className="relative inline-flex rounded-full h-5 w-5 bg-sky-500 items-center justify-center text-white text-[9px]">
                             10
                         </span>
                     </span>
