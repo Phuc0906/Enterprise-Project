60b8742ebe68d725f83237aed18c4c7e1d9121e7
Hien <s3877996@rmit.edu.vn>
2023-05-01 22:22:58 +0700

    Add Footer.js and Cart.js

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,6 +1,6 @@
 import React , { useContext }from "react";
 import {useParams} from "react-router-dom";
-
+import { Outlet, Link } from "react-router-dom";
 import Wrapper from "../components/Wrapper";
 import NavBar from "../components/NavBar";
 import ProductDetailsCarousel from "../components/ProductDetailsCarousel";
@@ -48,10 +48,19 @@ return (
                                     </div>
                                     <div className="text-sm md:text-md py-5 border-t mt-5 text-white">The subtotal reflects the total price of your order before any applicable discounts. It does not include shipping costs and taxes</div>
                                 </div>
-                                <button className="w-full mt-5 bg-black text-white py-4 rounded-full text:lg font-medium trasition-transform active:scale-95 mb-3 hover:opacity-75">Check out</button>
+                                <button className="w-full mt-5 bg-black text-white py-4 rounded-full text:lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">Check out</button>
                             </div>
                         </div>
 
+                    {/*If the cart is empty*/}
+                    <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+                        <img src="https://www.99fashionbrands.com/wp-content/uploads/2020/12/empty_cart.png" width={500} height={500} className="w-[500px] md:w-[400px]"/>
+                        <span className="text-xl font-bold">Look like your cart is empty</span>
+                        <span className="text-center mt-4">Go ahead and explore top categories</span>
+                        <Link to="/products">
+                            <button className="w-full px-8 mt-8 bg-black text-white py-4 rounded-full text:lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">Shopping</button>
+                        </Link>
+                    </div>
 
                 </Wrapper>
             </div>
