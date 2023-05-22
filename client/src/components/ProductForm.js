1a5ca86c9d6d32bded3904e686729a346ff3e86a
Phuc Hoang <phuchoang022002@gmail.com>
2023-05-22 13:33:13 +0700

    Product Form

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -322,7 +322,7 @@ const ProductForm = () => {
         } else {
             uploadProduct();
         }
-        location.reload();
+        window.location.reload();
     };
 
     const sizeArrowHandle = () => {
@@ -610,7 +610,7 @@ const ProductForm = () => {
                         type="click"
                         onClick={handleSubmitForm}
                         className="m-3 pt-3 inline-block rounded-full bg-blue-800 px-6 pb-2 text-white text-sm font-medium uppercase leading-normal  shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)]">
-                        Add Product
+                        {(location.state.update) ? "Save" : "Add Product"}
                     </button>
                     <button
                         type="click"
